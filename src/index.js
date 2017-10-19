312ce194ff74fbc5e5932323f389df29765a74f0
MooYeol Prescott Lee <mooyoul@gmail.com>
2017-10-20 01:37:50 +0900

    add `Joi.date().timestamp()` support

--- src/index.js
+++ src/index.js
@@ -33,7 +33,12 @@ let TYPES = {
     return schema;
   },
 
-  date: (schema) => {
+  date: (schema, joi) => {
+    if (joi._flags.timestamp) {
+      schema.type = 'integer';
+      return schema;
+    }
+
     schema.type = 'string';
     schema.format = 'date-time';
     return schema;
